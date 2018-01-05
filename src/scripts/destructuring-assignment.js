7c63074244bd751da18859f0e598f9a7c2bfca70
JasnaMRB <j.rodulfa@gmail.com>
2018-01-05 00:30:44 -0500

    More stuff to destructuring-assignment, replace `var` with `let`

--- src/scripts/destructuring-assignment.js
+++ src/scripts/destructuring-assignment.js
@@ -1,35 +1,35 @@
 /*
 to see the value of a field in an object in the console, typically you assign it inside a variable and reference it 
 */
-var obj = {
+let obj = {
     color: "blue"
 }
 
-console.log(obj.color); // blue
+console.log("obj.color:", obj.color); // obj.color: blue
 
 /*
     destructuring allows you to directly reference the field
 */
 
-var {color2} = {
+let {color2} = {
     color2: "red"
 }
 
-console.log(color2); // red
+console.log("color2:", color2); // color2: red
 
 /*
     Works with multiple fields
 */
 
-var {color3, position} = {
+let {color3, position} = {
     color3:   "orange",
     name:     "John",
     state:    "New York",
     position: "forward"
 }
 
-console.log(color3); // orange
-console.log(position); // forward
+console.log("color3:", color3); // color3: orange
+console.log("position:", position); // position: forward
 
 /*
  can use this with functions to only grab parts of a return value
@@ -44,14 +44,74 @@ function generateObj() {
     }
 }
 
-var {name, state} = generateObj();
+let {name, state} = generateObj();
 
-console.log(name); // Jill
-console.log(state); // Virginia
+console.log("name:", name); // name: Jill
+console.log("state:", state); // state: Virginia
 
 // can also rename them
+let {name:firstName, state:loc} = generateObj();
 
-var {name:firstName, state:loc} = generateObj();
+// aside: class originally had `state:location`, but this would
+// make my browser try to redirect to `src/Virginia`!
 
-console.log(firstName);
-console.log(loc);
+console.log("firstName:", firstName); // firstName: Jill
+console.log("loc:", loc); // loc: Virginia
+
+
+// If you want to pick and choose elements from a long array...
+
+
+let [first,,,,fifth] = ["red", "yellow", "green", "blue", "orange"];
+
+console.log("first:", first); // first: red
+console.log("fifth:", fifth); // fifth: orange
+
+// Some more crazy stuff
+
+let people = [
+  {
+    "firstName": "Clinton",
+    "lastName": "Ruiz",
+    "phone": "1-403-985-0449",
+    "email": "pharetra@facilisislorem.org",
+    "address": "Ap #829-3443 Nec St."
+  },
+  {
+    "firstName": "Skyler",
+    "lastName": "Carroll",
+    "phone": "1-429-754-5027",
+    "email": "Cras.vehicula.alique@diamProin.ca",
+    "address": "P.O. Box 171, 1135 Feugiat St."
+  },
+  {
+    "firstName": "Kylynn",
+    "lastName": "Madden",
+    "phone": "1-637-627-2810",
+    "email": "mollis.Duis@ante.co.uk",
+    "address": "993-6353 Aliquet, Street"
+  },
+  {
+    "firstName": "Chaney",
+    "lastName": "Edwards",
+    "phone": "1-397-181-4501",
+    "email": "rutrum@Nullamlobortis.net",
+    "address": "P.O. Box 342, 9574 Egestas Street"
+  }
+]
+
+people.forEach(({firstName}) => console.log("first name:", firstName));
+/* first name: Clinton
+*  first name: Skyler
+*  first name: Kylynn
+*  first name: Chaney
+*/
+
+
+let [,secondPers] = people; // get the second obj in the array
+
+function logEmail({email}) { // get the email property
+    console.log(email);
+}
+
+logEmail(secondPers); // Cras.vehicula.alique@diamProin.ca
